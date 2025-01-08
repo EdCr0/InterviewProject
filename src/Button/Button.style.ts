5d0ab03ac3e20f1d6b3e9b3f8ca8e9e7c122fef1
EdCr0 <73516539+EdCr0@users.noreply.github.com>
2025-01-08 17:04:55 -0600

    Myownbutoncomptotest

---
+++ src/Button/Button.style.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    mainButtonContainer: {
+        backgroundColor: '#0527FB',
+        borderRadius: 10,
+        padding: 8,
+        alignItems:'center'
+    },
+    buttonTitle:{
+        color: '#FFF',
+        fontWeight: "bold",
+        fontSize: 18
+    },
+});
